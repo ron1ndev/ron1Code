779cf3f6096fd88fe07a5cd6c1b48d66c82ffa56
Роман Громов <roma-gromov-1990@mail.ru>
2025-05-18 01:17:31 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Index from './components/defoult/index.vue';
+
+import { ref, computed } from 'vue';
+
+let text = 'Кнопка'
+
+const handleClick = (value)=>{
+  console.log(value)
+}
+
+</script>
+
+<template>
+ <div>
+  <Index/>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
