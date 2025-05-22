e6f28a8945204e4578d8cc3286e22065e40dccae
Роман Громов <roma-gromov-1990@mail.ru>
2025-05-23 01:42:34 +0500

    added sub-menu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,6 +53,34 @@ onUnmounted(()=>{
   
 })
 
+
+const menuLinks = [
+      {
+    icon: `              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M2 11.9999L11.6422 16.8211C11.7734 16.8866 11.839 16.9194 11.9078 16.9324C11.9687 16.9438 12.0313 16.9438 12.0922 16.9324C12.161 16.9194 12.2266 16.8866 12.3578 16.8211L22 11.9999M2 16.9999L11.6422 21.8211C11.7734 21.8866 11.839 21.9194 11.9078 21.9324C11.9687 21.9438 12.0313 21.9438 12.0922 21.9324C12.161 21.9194 12.2266 21.8866 12.3578 21.8211L22 16.9999M2 6.99994L11.6422 2.17883C11.7734 2.11324 11.839 2.08044 11.9078 2.06753C11.9687 2.0561 12.0313 2.0561 12.0922 2.06753C12.161 2.08044 12.2266 2.11324 12.3578 2.17883L22 6.99994L12.3578 11.8211C12.2266 11.8866 12.161 11.9194 12.0922 11.9324C12.0313 11.9438 11.9687 11.9438 11.9078 11.9324C11.839 11.9194 11.7734 11.8866 11.6422 11.8211L2 6.99994Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>`,
+    title:'Проекты'        
+  },
+  {
+    icon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 21L8 7C8 6.07003 8 5.60504 8.10222 5.22354C8.37962 4.18827 9.18827 3.37962 10.2235 3.10222C10.605 3 11.07 3 12 3C12.93 3 13.395 3 13.7765 3.10222C14.8117 3.37962 15.6204 4.18827 15.8978 5.22354C16 5.60504 16 6.07003 16 7V21M5.2 21L18.8 21C19.9201 21 20.4802 21 20.908 20.782C21.2843 20.5903 21.5903 20.2843 21.782 19.908C22 19.4802 22 18.9201 22 17.8V10.2C22 9.07989 22 8.51984 21.782 8.09202C21.5903 7.71569 21.2843 7.40973 20.908 7.21799C20.4802 7 19.9201 7 18.8 7L5.2 7C4.07989 7 3.51984 7 3.09202 7.21799C2.71569 7.40973 2.40973 7.71569 2.21799 8.09202C2 8.51984 2 9.07989 2 10.2L2 17.8C2 18.9201 2 19.4802 2.21799 19.908C2.40973 20.2843 2.71569 20.5903 3.09202 20.782C3.51984 21 4.0799 21 5.2 21Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`,
+    title:'Опыт'        
+  },
+    {
+    icon: `                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M22 9H2M14 17.5L16.5 15L14 12.5M10 12.5L7.5 15L10 17.5M2 7.8L2 16.2C2 17.8802 2 18.7202 2.32698 19.362C2.6146 19.9265 3.07354 20.3854 3.63803 20.673C4.27976 21 5.11984 21 6.8 21H17.2C18.8802 21 19.7202 21 20.362 20.673C20.9265 20.3854 21.3854 19.9265 21.673 19.362C22 18.7202 22 17.8802 22 16.2V7.8C22 6.11984 22 5.27977 21.673 4.63803C21.3854 4.07354 20.9265 3.6146 20.362 3.32698C19.7202 3 18.8802 3 17.2 3L6.8 3C5.11984 3 4.27976 3 3.63803 3.32698C3.07354 3.6146 2.6146 4.07354 2.32698 4.63803C2 5.27976 2 6.11984 2 7.8Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`,
+    title:'Технологии'        
+  },
+    {
+    icon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2 12H5.88197C6.56717 12 7.19357 12.3871 7.5 13C7.80643 13.6129 8.43283 14 9.11803 14H14.882C15.5672 14 16.1936 13.6129 16.5 13C16.8064 12.3871 17.4328 12 18.118 12H22M2 12V8.8C2 7.11984 2 6.27976 2.32698 5.63803C2.6146 5.07354 3.07354 4.6146 3.63803 4.32698C4.27976 4 5.11984 4 6.8 4H17.2C18.8802 4 19.7202 4 20.362 4.32698C20.9265 4.6146 21.3854 5.07354 21.673 5.63803C22 6.27976 22 7.11984 22 8.8V12M2 12V15.2C2 16.8802 2 17.7202 2.32698 18.362C2.6146 18.9265 3.07354 19.3854 3.63803 19.673C4.27976 20 5.11984 20 6.8 20H17.2C18.8802 20 19.7202 20 20.362 19.673C20.9265 19.3854 21.3854 18.9265 21.673 18.362C22 17.7202 22 16.8802 22 15.2V12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>`,
+    title:'Контакты'        
+  },
+]
+
 </script>
 
 <template>
@@ -66,7 +94,7 @@ onUnmounted(()=>{
                       {{ printText.dev }}
                   </div>
                   <div class="hero__title-app">
-                      <span class="hero__title-gradient"><span class="k1">Web Apps</span> <span class="k2">Websites</span></span>
+                      <span class="hero__title-gradient"><span class="k1">Web Apps</span> & <span class="k2">Websites</span></span>
                   </div>                  
                    <div class="hero__subtitle">UI для сайтов и приложений</div>
               </div>
@@ -103,7 +131,7 @@ onUnmounted(()=>{
                             </svg>
                   </a>
                   </div>
-                  <div class="hero__tg">
+                  <!-- <div class="hero__tg">
                       <a href="https://t.me/R_0N11" target="_blank">
                           <svg class="logo__icon-svg tg" width="40" height="40" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                               <defs>
@@ -115,14 +143,14 @@ onUnmounted(()=>{
                               </defs>
                               <path
                                 class="logo__icon-path"
-                                fill="url(#telegram-gradient)"
+                                fill="currentColor"
                                 fill-rule="evenodd"
                                 clip-rule="evenodd"
                                 d="M32 0C14.3215 0 0 14.3236 0 32C0 49.6764 14.3236 64 32 64C49.6784 64 64 49.6764 64 32C64 14.3236 49.6764 0 32 0ZM47.7172 21.9231L42.465 46.6725C42.0769 48.4274 41.0323 48.8526 39.5747 48.0268L31.5747 42.1306L27.7161 45.8467C27.2908 46.272 26.9295 46.6333 26.1037 46.6333L26.6715 38.4908L41.4968 25.0963C42.143 24.5285 41.3543 24.2065 40.5017 24.7742L22.1791 36.3086L14.2823 33.8436C12.5667 33.3027 12.5275 32.128 14.6436 31.3022L45.4957 19.4044C46.9285 18.8883 48.1796 19.7533 47.7151 21.921L47.7172 21.9231Z"
                               />
                             </svg>
                   </a>
-                  </div>
+                  </div> -->
               </div>
           </div>
           <!-- <div class="hero__img-code1"><img src="../assets/img/home/code2.png" alt=""></div>
@@ -139,6 +167,16 @@ onUnmounted(()=>{
 
     <Skills/>
 
+    <nav class="header__menu-mobile menu-mobile">
+      <ul class="menu-mobile__list">
+          <li class="menu-mobile__item" v-for="(item,index) in menuLinks" :key="index">
+              <div class="menu-mobile__icon" v-html="item.icon">
+               </div>
+              <div class="menu-mobile__title">{{ item.title }}</div>
+          </li>
+      </ul>
+  </nav>
+
   </main>
 </template>
 
@@ -152,6 +190,54 @@ onUnmounted(()=>{
 
 
 <style lang="scss" scoped>
+
+main{
+  min-height: 100vh;
+  position: relative;
+}
+
+.header__menu-mobile{
+  display: none;
+@media (max-width: #{$md3}px) {
+  display: block;
+}
+  position: absolute;
+  
+  width: 100%;
+  bottom: 0;
+  left: 0;
+}
+.menu-mobile__icon{
+svg{
+  width: 24px;
+  height: 24px;
+  color: #fff;
+}
+}
+.menu-mobile__title{
+font-size: 11px;
+}
+
+.menu-mobile__list{
+  border-top:1px solid #1a1d24;
+  padding: 10px 20px 10px 20px;
+  display: flex;
+  align-items:center;
+  justify-content: space-between;
+}
+
+.menu-mobile__item{
+
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  justify-content: center;
+  align-items:center;
+
+}
+
+
 // MAIN HERO
 .hero__btns{
   display: flex;
@@ -166,24 +252,25 @@ onUnmounted(()=>{
 ._container {
 }
 .hero {
-  padding-top: 100px;
+  padding-top: 200px;
   // margin-top: 170px;
   // margin-bottom: 120px;
   // min-height: calc(100vh); // Высота устроства - отступ сверху - отступ от последующего блока
 
   @media (max-width: #{$md1}px) {
     // min-height: calc(100vh);
+    padding-top: 160px;
   }
 
   @media (max-width: #{$md3}px) {
    
-    padding-top: 160px;
+    padding-top: 120px;
     
     // min-height: calc(100vh);
   }
 }
 .hero__content {
-  margin:100px 0px 0px 0px;
+  // margin:100px 0px 0px 0px;
   text-align:center;
   display: flex;
   justify-content: center;
@@ -198,20 +285,40 @@ onUnmounted(()=>{
   // max-width: 650px;
   line-height: 78px;
  font-family: 'Montserrat';
+ @media (max-width: #{$md1}px) {
+  font-size: 72px;
+ }
+  @media (max-width: #{$md2}px) {
+  font-size: 62px;
+ }
  @media (max-width: #{$md3}px) {
   min-width: 300px;
   font-size: 50px;
   line-height: 58px;
-    }
+  }
+  @media (max-width:600px) {
+    font-size: 34px;
+    line-height: 48px;
+  }
 
 }
 
 .hero__subtitle{
   font-size: 30px;
   font-weight: 700;
+    @media (max-width: #{$md1}px) {
+    font-size: 26px;
+    }
+    @media (max-width: #{$md2}px) {
+     font-size: 22px;
+    }
+    @media (max-width: #{$md3}px) {
+     font-size: 16px;
+    }
+    
 } 
 .hero__title-dev{
-  min-height: 100px;
+  // min-height: 100px;
 }
 
 
@@ -407,6 +514,11 @@ z-index: 10;
 }
 .hero__tg {
   margin-bottom: 20px;
+  
+
+  svg{
+  color: white;
+  }
 }
 .hero__img-code1 {
   position: absolute;
