0004e5d5f6afe2bbc8cbcb441cc41caf06f0b5f2
Роман Громов <roma-gromov-1990@mail.ru>
2025-06-06 21:58:10 +0500

    configured deploy

--- src/components/skills/skills.vue
+++ src/components/skills/skills.vue
@@ -1,16 +1,25 @@
 <script setup>
 
+const images = import.meta.glob('@/assets/img/skills/*.{png,svg}', {
+  eager: true,
+  as: 'url'
+})
+
 const skills = [
-  {path:'/src/assets/img/skills/1.png',name:'CSS 3',alt:'CSS 3'},
-  {path:'/src/assets/img/skills/2.png',name:'HTML 5',alt:'HTML 5'},
-  {path:'/src/assets/img/skills/3.png',name:'JavaScript',alt:'JavaScript'},
-  {path:'/src/assets/img/skills/4.1.png',name:'React',alt:'React'},
-  {path:'/src/assets/img/skills/8.png',name:'Vue',alt:'Vue'},
-  {path:'/src/assets/img/skills/5.png',name:'Git',alt:'Git'},
-  {path:'/src/assets/img/skills/9.svg',name:'Vite',alt:'Vite'},
-  {path:'/src/assets/img/skills/6.png',name:'Gulp',alt:'Gulp'},
+  {file:'1.png',name:'CSS 3',alt:'CSS 3'},
+  {file:'2.png',name:'HTML 5',alt:'HTML 5'},
+  {file:'3.png',name:'JavaScript',alt:'JavaScript'},
+  {file:'4.1.png',name:'React',alt:'React'},
+  {file:'8.png',name:'Vue',alt:'Vue'},
+  {file:'5.png',name:'Git',alt:'Git'},
+  {file:'9.svg',name:'Vite',alt:'Vite'},
+  {file:'6.png',name:'Gulp',alt:'Gulp'},
   // {path:'/src/assets/img/skills/7.png',name:'Webpack',alt:'Webpack'},
 ]
+
+skills.forEach(skill => {
+  skill.path = images[`/src/assets/img/skills/${skill.file}`]
+})
 
 </script>
 
